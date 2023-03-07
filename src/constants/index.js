eedb4ca58fc0c448fad6817e83b8854e987f1088
Willie van Staden <60608218+willivstn@users.noreply.github.com>
2023-03-07 14:06:47 +0200

    "small changes"

--- src/constants/index.js
+++ src/constants/index.js
@@ -202,16 +202,16 @@ const testimonials = [
 
 const projects = [
   {
-    name: 'Car Rent',
+    name: 'Excercise Web App',
     description:
-      'Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.',
+      'Web-based platform that allows users to search for exercises based on a specific body part as well as view the exercise with youtube',
     tags: [
       {
         name: 'react',
         color: 'blue-text-gradient',
       },
       {
-        name: 'mongodb',
+        name: 'restapi',
         color: 'green-text-gradient',
       },
       {
@@ -223,16 +223,16 @@ const projects = [
     source_code_link: 'https://github.com/',
   },
   {
-    name: 'Job IT',
+    name: 'Personal portfolio web app',
     description:
-      'Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.',
+      'Personal portfolio web application with fluent animations to showcase skills to future employees as well as a backend to upload projects.',
     tags: [
       {
         name: 'react',
         color: 'blue-text-gradient',
       },
       {
-        name: 'restapi',
+        name: 'mongodb',
         color: 'green-text-gradient',
       },
       {
@@ -244,21 +244,25 @@ const projects = [
     source_code_link: 'https://github.com/',
   },
   {
-    name: 'Trip Guide',
+    name: 'Full stack e-commerce web app',
     description:
-      'A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.',
+      'A comprehensive e-commerce web application for 3-D printed projects, users can add products to a cart and purchase them via paypal. The web app has admin features to complete, change and add product orders',
     tags: [
       {
-        name: 'nextjs',
+        name: 'nodejs',
         color: 'blue-text-gradient',
       },
       {
-        name: 'supabase',
+        name: 'mongodb',
         color: 'green-text-gradient',
       },
       {
         name: 'css',
         color: 'pink-text-gradient',
+      },
+      {
+        name: 'react',
+        color: 'blue-text-gradient',
       },
     ],
     image: e_commerce_fullstack,
